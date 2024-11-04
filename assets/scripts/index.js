9f86dee637e7a1912fa194acc67ca0f83c0a8c66
ArefAH <arefabouhamdan@gmail.com>
2024-11-04 22:19:38 +0200

    Created inputChange to store input and toggleModal to show/hide the modal

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,13 +1,43 @@
-addIncome.addEventListener('click',()=>{
-    incomeModal.classList.toggle("hidden")
-})
-addExpense.addEventListener('click',()=>{
-    expenseModal.classList.toggle("hidden")
-})
+let ballance = 0;
+let income = 0;
+let expense = 0;
+let incomeData = {
+  name: "",
+  amount: 0,
+  category: "",
+  date: "",
+};
+let expenseData = {
+  name: "",
+  amount: 0,
+  category: "",
+  date: "",
+};
 
-expenseCancel.addEventListener('click', ()=>{
-    expenseModal.classList.toggle("hidden")
-})
-incomeCancel.addEventListener('click', ()=>{
-    incomeModal.classList.toggle("hidden")
-})
+function inputChange(inputElement, storage, key) {
+  inputElement.addEventListener("change", (event) => {
+    storage[key] = event.target.value;
+  });
+}
+
+function toggleModal(button, modal) {
+  button.addEventListener("click", () => {
+    modal.classList.toggle("hidden");
+  });
+}
+
+toggleModal(addIncome, incomeModal);
+toggleModal(addExpense, expenseModal);
+toggleModal(expenseCancel, expenseModal);
+toggleModal(incomeCancel, incomeModal);
+toggleModal(incomeButton, incomeModal);
+
+
+inputChange(incomeName, incomeData, "name");
+inputChange(incomeAmount, incomeData, "amount");
+inputChange(incomeCategory, incomeData, "category");
+inputChange(incomeDate, incomeData, "date");
+inputChange(expenseName, expenseData, "name");
+inputChange(expenseAmount, expenseData, "amount");
+inputChange(expenseCategory, expenseData, "category");
+inputChange(expenseDate, expenseData, "date");
