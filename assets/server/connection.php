ed6b7779f3c8601e4f46f84af0041b7a597a56e8
ArefAH <arefabouhamdan@gmail.com>
2024-11-09 02:43:13 +0200

    PHP - Created login file

--- assets/server/connection.php
+++ assets/server/connection.php
@@ -1,4 +1,9 @@
 <?php
+
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Methods: POST, GET");
+
 $host = 'localhost';
 $username = 'root';
 $pass = '';
@@ -9,5 +14,7 @@ $connection = new mysqli($host, $username, $pass, $dbname);
 if($connection->connect_error){
     die('Connection Error');
 }
+
+
 
 ?>
